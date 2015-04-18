16d68447d7cb79f844c97c479ad7f210254d285c
andro <andro999b@gmail.com>
2015-04-18 20:31:00 +0300

    log pattern

--- src/main/java/com/lardi_trans/http/service/utils/HtmlAppender.java
+++ src/main/java/com/lardi_trans/http/service/utils/HtmlAppender.java
@@ -4,14 +4,17 @@ import ch.qos.logback.classic.html.HTMLLayout;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
 import ch.qos.logback.core.helpers.CyclicBuffer;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * Created by Andrey on 18.04.2015.
  */
 public class HtmlAppender extends AppenderBase<ILoggingEvent> {
     public static final int MAX_RECORDS = 150;
+    public static final String DEFAULT_CONVERSION_PATTERN = "%date%level%logger{36}%msg";
 
     private CyclicBuffer<ILoggingEvent> cb = new CyclicBuffer<>(MAX_RECORDS);
+    private String pattern;
 
     @Override
     protected void append(ILoggingEvent event) {
@@ -19,10 +22,18 @@ public class HtmlAppender extends AppenderBase<ILoggingEvent> {
         cb.add(event);
     }
 
+    @Override
+    public void start() {
+        super.start();
+
+        if (StringUtils.isEmpty(pattern))
+            pattern = DEFAULT_CONVERSION_PATTERN;
+    }
+
     public String print() {
         StringBuilder sb = new StringBuilder();
         HTMLLayout layout = new HTMLLayout();
-        layout.setPattern("%date%level%logger%msg");
+        layout.setPattern(pattern);
         layout.setContext(getContext());
         layout.start();
 
@@ -35,5 +46,9 @@ public class HtmlAppender extends AppenderBase<ILoggingEvent> {
         sb.append(layout.getFileFooter());
 
         return sb.toString();
+    }
+
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
     }
 }
