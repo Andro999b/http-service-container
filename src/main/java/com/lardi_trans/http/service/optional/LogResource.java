b46911b957506bb8d488fcf0f8f3353a30671d62
Stanislav Koval <stastbk@gmail.com>
2015-05-18 09:36:06 +0300

    add UTF-8 charset for a log resource

--- src/main/java/com/lardi_trans/http/service/optional/LogResource.java
+++ src/main/java/com/lardi_trans/http/service/optional/LogResource.java
@@ -2,13 +2,16 @@ package com.lardi_trans.http.service.optional;
 
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
+
 import com.lardi_trans.http.service.api.annotation.ApiIgnore;
 import com.lardi_trans.http.service.utils.HtmlAppender;
+
 import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  * Created by Andrey on 18.04.2015.
@@ -17,8 +20,9 @@ import javax.ws.rs.Produces;
 @ApiIgnore
 public class LogResource {
     @GET
-    @Produces({"text/html"})
+    @Produces({MediaType.TEXT_HTML + ";charset=utf-8"})
     public String getLog() {
+        
         LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
         Logger logger = loggerContext.getLogger(Logger.ROOT_LOGGER_NAME);
 
